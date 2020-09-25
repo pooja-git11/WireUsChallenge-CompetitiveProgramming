47791aa2752910f83442cdbd7b752e9cdd1edf80
Pooja <pooja016mca19@igdtuw.ac.in>
2020-09-25 22:43:01 +0530

    day8

---
+++ Day-008/string.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<unordered_map>
+#include<vector>
+using namespace std;
+
+vector<int>& checkForOccurance(vector<string>& ip, vector<string>& str)
+{
+    unordered_map<string, int> mp;
+    vector<int> res;
+
+    for(int i = 0; i < ip.size(); i++)
+        mp[ip[i]]++;
+
+   for (auto itr = mp.begin(); itr != mp.end(); ++itr) {
+       cout << '\t' << itr->first
+             << '\t' << itr->second << '\n';
+    }
+
+
+   for(int i = 0; i < str.size(); i++)
+    {
+        for(auto itr = mp.begin(); itr != mp.end(); ++itr)
+        {
+
+            if(!(str[i].compare(itr -> first)))
+            {
+                res.push_back(itr -> second);
+                break;
+            }
+        }
+
+    }
+
+    return res;
+
+}
+
+int main()
+{
+    int n, m;
+
+    cout << "Enter no. of input string ";
+    cin >> n;
+
+    vector<string>ip(n);
+    cout << "Enter String ";
+    for(int i = 0; i < n; i++)
+         cin >> ip[i];
+
+    cout << "Enter no. of ouput string  ";
+    cin >> m;
+    vector<string>str(m);
+    for(int i = 0; i < m; i++)
+        cin >> str[i];
+
+    vector<int> res = checkForOccurance(ip, str);
+
+    for(int i = 0; i < res.size(); i++)
+        cout << res[i];
+}
